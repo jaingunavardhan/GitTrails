691a1876b7020a670536515c88c38f70b910f1de
jaingunavardhan <jaingunavardhan@gmail.com>
2024-01-30 09:19:04 +0530

    Example JS of getElementsByClassName

---
+++ byClass.js
@@ -0,0 +1,9 @@
+const fruitlist = document.getElementsByClassName('fruit');
+//Here we will have all the classes with name FRUIT and hence we get list of it
+//So, we used getElements (ELEMENTS not ELEMENT)
+fruitlist[2].style.backgroundColor = 'yellow';
+
+for(var i=0; i<fruitlist.length; i++)
+  {
+    fruitlist[i].style.fontWeight = 'bold';
+  }
