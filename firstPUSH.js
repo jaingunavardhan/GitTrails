1c9835afe98dd3de981b126e9273fe77efffa0f4
jaingunavardhan <jaingunavardhan@gmail.com>
2024-01-30 08:10:41 +0530

    JS code with steps to PUSH your file

---
+++ firstPUSH.js
@@ -0,0 +1,8 @@
+console.log("Steps to PUSH first file to JS:");
+console.log("1. Initialise git : git init")
+console.log("2. Add your file: git add 'file name'");
+console.log("3. commit your file: git commit -m 'Commit comments goes here'");
+console.log("4. Specify branch : git branch -M 'main/any branch'");
+console.log("5. Add the remote otigin repository path in github :")
+console.log("   git remote add origin YOUR_REP_PATH_HERE");
+console.log("6. PUSH the commits (final step) : git push -u origin BRANCH_NAME");
