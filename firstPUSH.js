a7d6787cc580469da04fb6eb1f86202069f7c0ec
jaingunavardhan <jaingunavardhan@gmail.com>
2024-01-30 08:12:30 +0530

    JS code with steps to PUSH your file

--- firstPUSH.js
+++ firstPUSH.js
@@ -6,3 +6,5 @@ console.log("4. Specify branch : git branch -M 'main/any branch'");
 console.log("5. Add the remote otigin repository path in github :")
 console.log("   git remote add origin YOUR_REP_PATH_HERE");
 console.log("6. PUSH the commits (final step) : git push -u origin BRANCH_NAME");
+
+console.log("for subsequent pushes, just skip step-1");
