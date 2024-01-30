aa535238fa93742568d23bf14a8633e68052b2d6
jaingunavardhan <jaingunavardhan@gmail.com>
2024-01-30 08:03:27 +0530

    First Commit: JS code with GIT configuration steps in TERMINAL

---
+++ configureGIT.js
@@ -0,0 +1,6 @@
+console.log("How to configure GIT in terminal ? Let's see:");
+console.log("1. Open TERMINAL")
+console.log("2. Congigure your USERNAME using the following code:")
+console.log('   git config --global user.name "jaingunavardhan"');
+console.log("3. Followed by email configuration as follows:");
+console.log('   git config --global user.email "jaingunavardhan@gmail.com"');
