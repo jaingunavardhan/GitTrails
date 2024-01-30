672c9d7c33bf9034fed3d9955876c5503cddcba0
jaingunavardhan <jaingunavardhan@gmail.com>
2024-01-30 08:56:36 +0530

    Example JS of getElementByID

---
+++ byID.js
@@ -0,0 +1,13 @@
+const mainheading = document.getElementById("main-heading");
+mainheading.textContent = 'Fruit World'; //1-Answer
+mainheading.style.color = 'orange'; //2-Answer
+
+const divheader = document.getElementById("header");
+divheader.style.backgroundColor = "green"; //3-Answer
+divheader.style.borderBottom = '3px solid orange'; //4-Answer
+
+const fruitsbasket = document.getElementById("basket-heading");
+fruitsbasket.style.color = 'green'; //5-Answer
+
+const thanks = document.getElementById("thanks");
+thanks.innerHTML = '<p>Please visit us again</p>' //6-Answer
