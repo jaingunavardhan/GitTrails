4baa997475d61cdfc0ffa3b2a70bc88631968d97
jaingunavardhan <jaingunavardhan@gmail.com>
2024-03-12 04:36:38 +0530

    When server is called - Creating a form and taking a message, then storing it in a text file, displaying all messages in file every time

---
+++ serverFormFileMsgs.js
@@ -0,0 +1,57 @@
+const http = require('http');
+const fs = require('fs');
+//const { parse } = require('path');
+
+const server = http.createServer( (request, response)=>{
+    response.setHeader('content-type', 'text/html');
+    function showForm(response)
+    {
+        response.write('<html>');
+        response.write('<head> <title>Home Page</title> </head>')
+        response.write('<body>');
+        response.write(" <form action='/message' method='POST'> ");
+        response.write("<input type='text' name='message'>");
+        response.write("<button type='submit'>Send</button> ");
+        return response.end();
+    }
+
+    if(request.url == '/')
+    {
+        if( fs.existsSync('message.txt') )
+        {
+            const file = fs.readFileSync('message.txt')
+            response.write(file.toString());  
+            showForm(response);                    
+        }
+        
+    }
+    if(request.url == '/message')
+    {
+        const rawBody = [];                
+        
+        request.on('data', (chunk)=>{
+            rawBody.push(chunk);
+        });
+
+        return request.on('end', ()=>{
+            const rawBodyConactenated = Buffer.concat(rawBody);
+            const parsedBodyString = rawBodyConactenated.toString();
+            const message = parsedBodyString.split('=')[1];
+            fs.appendFile('message.txt', message, (error)=>{
+                if(error)
+                    console.log("File append error")
+                else
+                {                                                   
+                    response.statusCode=302;
+
+                    const file = fs.readFileSync('message.txt')
+                    response.write(file.toString());  
+
+                    showForm(response);                                                                            
+                }
+            })
+        })
+    }
+})
+
+server.listen(4000);
