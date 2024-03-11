c0fe1ee777d262354eb13233468849b698396c99
jaingunavardhan <jaingunavardhan@gmail.com>
2024-03-12 05:17:01 +0530

    When server is called - Creating a form and taking a message, then storing it in a text file, displaying all messages in file every time using own file as Module

--- serverFormFileMsgs.js
+++ serverFormFileMsgs.js
@@ -1,57 +1,11 @@
 const http = require('http');
-const fs = require('fs');
-//const { parse } = require('path');
+const handler = require('./serverFormFileModule.js');
+// "./FILE" indicates relative path to the current file and also
+// informs that this is not a JS core module but is user-defined
 
-const server = http.createServer( (request, response)=>{
-    response.setHeader('content-type', 'text/html');
-    function showForm(response)
-    {
-        response.write('<html>');
-        response.write('<head> <title>Home Page</title> </head>')
-        response.write('<body>');
-        response.write(" <form action='/message' method='POST'> ");
-        response.write("<input type='text' name='message'>");
-        response.write("<button type='submit'>Send</button> ");
-        return response.end();
-    }
-
-    if(request.url == '/')
-    {
-        if( fs.existsSync('message.txt') )
-        {
-            const file = fs.readFileSync('message.txt')
-            response.write(file.toString());  
-            showForm(response);                    
-        }
-        
-    }
-    if(request.url == '/message')
-    {
-        const rawBody = [];                
-        
-        request.on('data', (chunk)=>{
-            rawBody.push(chunk);
-        });
-
-        return request.on('end', ()=>{
-            const rawBodyConactenated = Buffer.concat(rawBody);
-            const parsedBodyString = rawBodyConactenated.toString();
-            const message = parsedBodyString.split('=')[1];
-            fs.appendFile('message.txt', message, (error)=>{
-                if(error)
-                    console.log("File append error")
-                else
-                {                                                   
-                    response.statusCode=302;
-
-                    const file = fs.readFileSync('message.txt')
-                    response.write(file.toString());  
-
-                    showForm(response);                                                                            
-                }
-            })
-        })
-    }
-})
+const server = http.createServer(handler);
+//We are calling back handler when any request is made by client
+// NOTE: just pass the function reference here as this is callback
+// You can also pass entire function definition but not function call with ().
 
 server.listen(4000);
