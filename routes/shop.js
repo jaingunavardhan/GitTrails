c1ecc780d1e7391425eba01f3407c28ececa097e
jaingunavardhan <jaingunavardhan@gmail.com>
2024-03-15 14:30:29 +0530

    Shopping App's shopping page user's url routing file with clean code

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (request, response, next)=>{
     console.log("In the Home Shopping Site page")
-    response.send("<h1>Welcome to Shopping Site</h1>");
+    response.sendFile( path.join(__dirname, '..', 'views', 'shop.html') );
 })
 
 module.exports = router;
